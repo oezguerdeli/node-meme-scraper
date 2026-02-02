be8127b0f5c581e281cacb5593449529beb4c90a
Özgür Deli <oezguer.deli@gmail.com>
2026-02-02 20:07:37 +0100

    Changing non optional fixes

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { argv } from 'node:process';
 
-//  import crypto from 'crypto';
-
 //  URL for scraping memes
 const scrapeSource = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 let digitCount = 0;
-let bottomText = '';
-let topText = '';
-let memePicture = '';
-
-//  let tempArrayHash = [];
 
 //  if folder 'memes' doesn´t exist, create one
 if (!fs.existsSync('memes')) {
@@ -20,92 +13,40 @@ if (!fs.existsSync('memes')) {
 } else {
   //  check existing files to make sure that id is incremented correctly
   const files = fs.readdirSync('memes');
-
-  files.forEach((f) => {
-    let maxCount = 0;
-
-    const number = parseInt(f);
-
-    if (number > maxCount) {
-      maxCount = number;
-    } else {
-      //  debugging
-      console.log(maxCount);
-      digitCount = maxCount + 1;
-    }
-  });
 }
 
-//  check if there are arguments when running index.js
-//  and make sure they are complete (top, bottom, picture)
-if (argv.length > 4) {
-  //  assign arguments to variables
-  bottomText = argv[2];
-  topText = argv[3];
-  memePicture = argv[4];
+//  no arguments
+const resp = await fetch(scrapeSource);
+const html = await resp.text();
 
-  const sourceUrl =
-    'https://memecomplete.com/edit/images/' +
-    memePicture +
-    '/' +
-    topText +
-    '/' +
-    bottomText +
-    '.jpg';
+const regex = /<img[^>]*src="([^"]+)"/g;
 
-  const resp = await fetch(sourceUrl);
-  const buffer = await resp.arrayBuffer();
+const imageUrls = [];
+let match;
 
-  const fileName = String(digitCount + 1).padStart(2, '0') + '.jpg';
+let progressCounter = 0;
+
+while ((match = regex.exec(html)) !== null) {
+  if (imageUrls.length === 10) {
+    break;
+  }
+
+  const cleanUrl = match[1].split('?')[0];
+  imageUrls.push(cleanUrl);
+  progressCounter += 10;
+  console.log(`Progress: ${progressCounter}%`);
+  console.log(cleanUrl);
+}
+
+for (let i = 0; i < imageUrls.length; i++) {
+  const imageUrl = imageUrls[i];
+  console.log(imageUrl);
+
+  const response = await fetch(imageUrl);
+  const buffer = await response.arrayBuffer();
+
+  const fileName = String(digitCount + i + 1).padStart(2, '0') + '.jpg';
   const filePath = path.join('memes', fileName);
+
   fs.writeFileSync(filePath, Buffer.from(buffer));
-} else {
-  //  no arguments
-  const resp = await fetch(scrapeSource);
-  const html = await resp.text();
-
-  const regex = /<img[^>]*src="([^"]+)"/g;
-
-  const imageUrls = [];
-  let match;
-
-  let progressCounter = 0;
-
-  while ((match = regex.exec(html)) !== null) {
-    if (imageUrls.length === 10) {
-      break;
-    }
-
-    const cleanUrl = match[1];
-    imageUrls.push(cleanUrl);
-    progressCounter += 10;
-    console.log(`Progress: ${progressCounter}%`);
-    console.log(cleanUrl);
-
-    //  await tempArrayHash.push(sha256FromUrl(cleanUrl));
-  }
-
-  //  console.log(tempArrayHash);
-
-  //  debugging
-  //  console.log(imageUrls);
-
-  for (let i = 0; i < imageUrls.length; i++) {
-    const imageUrl = imageUrls[i];
-    console.log(imageUrl);
-
-    const response = await fetch(imageUrl);
-    const buffer = await response.arrayBuffer();
-
-    const fileName = String(digitCount + i + 1).padStart(2, '0') + '.jpg';
-    const filePath = path.join('memes', fileName);
-
-    fs.writeFileSync(filePath, Buffer.from(buffer));
-  }
 }
-
-/*
-function sha256FromUrl(url) {
-  return crypto.createHash('sha256').update(url).digest('hex');
-}
-*/
